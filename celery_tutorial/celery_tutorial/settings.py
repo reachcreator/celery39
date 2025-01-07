cf732e9b4776e1a117bac485568004e55262254c
reachcreator <steve.reachcreator@gmail.com>
2025-01-07 17:14:00 +0000

    couldn't change the password to something django likes so url encoded it.

--- celery_tutorial/celery_tutorial/settings.py
+++ celery_tutorial/celery_tutorial/settings.py
@@ -17,8 +17,9 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Use the function
-CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL")
-CELERY_RESULT_BACKEND = os.environ.get("REDIS_HOST")
+# replace $ in the url with escaped $ that is URL encoded safely so the $ doesn't mess things up
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL").replace("$", "%24")
+CELERY_RESULT_BACKEND = os.environ.get("REDIS_HOST").replace("$", "%24")
 
 
 
