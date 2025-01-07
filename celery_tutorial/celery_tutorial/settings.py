fd1097a1a7aa0aa6efadfa9381477301011e15c3
reachcreator <steve.reachcreator@gmail.com>
2025-01-07 17:41:23 +0000

    took out perhaps superflous line ?

--- celery_tutorial/celery_tutorial/settings.py
+++ celery_tutorial/celery_tutorial/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Use the function
 # replace $ in the url with escaped $ that is URL encoded safely so the $ doesn't mess things up
 CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL").replace("$", "%24")
-CELERY_RESULT_BACKEND = "redis://" + os.environ.get("REDIS_HOST").replace("$", "%24")
-
 
 
 # Quick-start development settings - unsuitable for production
